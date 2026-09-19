cc48967b3b1a03380ef3821b9c3f91c1f916d4a0
Caio <caiooliver2007@hotmail.com>
2026-09-19 11:30:23 -0300

    feat: create new branch with code variation

--- Cap05/jogo-pedra-papel-tesoura.py
+++ Cap05/jogo-pedra-papel-tesoura.py
@@ -9,21 +9,9 @@ jogador2 = input("Insira o nome do segundo jogador: ")
 jogada1 = input(f"{jogador1}, faça a sua escolha: pedra, papel ou tesoura? ").strip().lower().replace(" ", "")
 jogada2 = input(f"{jogador2}, faça a sua escolha: pedra, papel ou tesoura? ").strip().lower().replace(" ", "")
 
-if jogada1 == "pedra" and jogada2 == "pedra":
+if jogada1 == jogada2:
     print("Empate! Jogue novamente.")
-elif jogada1 == "pedra" and jogada2 == "tesoura":
-    print(f"{jogador1} venceu! Pedra quebra a tesoura.")
-elif jogada1 == "pedra" and "jogada2" == "papel":
-    print(f"{jogador2} venceu! Papel cobre a pedra.")
-elif jogada1 == "papel" and jogada2 == "pedra":
-    print(f"{jogador1} venceu! Papel cobre a pedra.")
-elif jogada1 == "papel" and jogada2 == "papel":
-    print("Empate! Jogue novamente.")
-elif jogada1 == "papel" and jogada2 == "tesoura":
-    print(f"{jogador2} venceu! Tesoura corta o papel.")
-elif jogada1 == "tesoura" and jogada2 == "pedra":
-    print(f"{jogador2} venceu! Pedra quebra a tesoura.")
-elif jogada1 == "tesoura" and jogada2 == "papel":
-    print(f"{jogador1} venceu! Tesoura corta o papel.")
+elif jogada1 == "tesoura" and jogada2 == "papel" or jogada1 == "pedra" and jogada2 == "tesoura" or jogada1 == "papel" and jogada2 == "tesoura":
+    print(f"{jogador1} venceu!")
 else:
-    print("Empate! Jogue novamente!")
+    print(f"{jogador2} venceu!")
